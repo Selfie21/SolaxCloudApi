8e8a5a7525f8d8e2fdec9103bb6a5736d2ec0b1e
Selfie21 <pierre.brosemer@gmail.com>
2020-08-01 10:16:15 +0200

    added check for password

--- SolaxCrawler.py
+++ SolaxCrawler.py
@@ -7,29 +7,49 @@ class SolaxCrawler():
 
     def __init__(self):
         date_today = datetime.now().strftime('%Y-%m-%d')
-        self.original_url = 'https://www.solaxcloud.com/i18n/language.do?language=en_US&url=/views/index.jsp'
-        self.login_url = 'https://www.solaxcloud.com/login/login.do'
-        self.data_url = 'https://www.solaxcloud.com/userIndex/getCurrentData.do?currentTime=' + date_today + '+12%3A01%3A42'
-        self.logout_url = 'https://www.solaxcloud.com/login/loginOut.do'
+        core_url = 'https://www.solaxcloud.com'
+        self.original_url = core_url + '/i18n/language.do?language=en_US&url=/views/index.jsp'
+        self.login_url = core_url + '/login/login.do'
+        self.data_url = core_url + '/userIndex/getCurrentData.do'
+        self.backup_url = core_url + '/userIndex/getYield.do'
+        self.logout_url = core_url + '/login/loginOut.do'
 
         self.headers = {
             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/80.0.3987.132 Safari/537.36'
         }
 
+        self.data_params = {
+            ('currentTime', date_today + ' 12:01:42')
+        }
+
+        self.backup_params = {
+            ('reportType', '4'),
+            ('day', str(datetime.now().day)),
+            ('month', str(datetime.now().month)),
+            ('year', str(datetime.now().year)),
+            ('webTime', datetime.now().strftime('%Y,%m,%d'))
+        }
+
+
 
     def get_data(self, session):
-        data_response = session.post(self.data_url, headers=self.headers, verify=False)
+        data_response = session.post(self.data_url, params=self.data_params, headers=self.headers, verify=False)
 
         if not self.is_json(data_response.text):
             return 0, 0, 0
-        data = json.loads(data_response.text)
 
+        data = json.loads(data_response.text)
         daily_yield = data['todayYield']
-        monthly_yield = data['monthYield']
         yearly_yield = data['yearYield']
-        total_yield = data['totalYield']
         current_power = data['gridpower']
+
+        # the solax website sometimes has the bug where it shows the daily yield as yearly yield
+        # if that happens i take the yearly yield from another source
+        if daily_yield == yearly_yield:
+            data_response = session.post(self.backup_url, params=self.backup_params, headers=self.headers, verify=False)
+            data = json.loads(data_response.text)
+            yearly_yield = self.find_year_data(data['returnList'], 2020)
 
         # close session
         requests.get(self.logout_url, verify=False)
@@ -47,14 +67,20 @@ class SolaxCrawler():
 
     def is_json(self, json_file):
         try:
-            json_object = json.loads(json_file)
+            json.loads(json_file)
         except ValueError as e:
             return False
         return True
 
+    def find_year_data(self, list_data, year):
+        for dataset in list_data:
+            if dataset['time'] == year:
+                return dataset['yieldtoday']
+        return 0
 
-username = 'user'
-password = '5f4dcc3b5aa765d61d8327deb882cf99'
+
+username = 'test'
+password = '098f6bcd4621d373cade4e832627b4f6'
 
 solaxretriever = SolaxCrawler()
 solax_session = solaxretriever.initiate_login_session(username, password)
